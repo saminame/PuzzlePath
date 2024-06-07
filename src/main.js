0844e26bbd2fd9aa1de5562135ac1c32254b8519
saminame <151252937+saminame@users.noreply.github.com>
2024-06-07 14:28:48 -0700

    Update main.js
    added TitleScene to scenes

--- src/main.js
+++ src/main.js
@@ -1,14 +1,3 @@
-// Jim Whitehead
-// Created: 4/14/2024
-// Phaser: 3.70.0
-//
-// Cubey
-//
-// An example of putting sprites on the screen using Phaser
-// 
-// Art assets from Kenny Assets "Shape Characters" set:
-// https://kenney.nl/assets/shape-characters
-
 // debug with extreme prejudice
 "use strict"
 
@@ -31,7 +20,7 @@ let config = {
     },
     width: 1440,
     height: 900,
-    scene: [Load, Platformer, Health]
+    scene: [TitleScene, Load, Platformer, Health]
 }
 
 var cursors;
