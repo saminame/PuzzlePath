3f9c942642e69d98c7e2dfd82b3254eae2615a02
saminame <151252937+saminame@users.noreply.github.com>
2024-06-07 14:31:45 -0700

    Update Platformer.js
    added the objects for lock, key, coin, ropes, ladder, and power ups

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -2,7 +2,6 @@ class Platformer extends Phaser.Scene {
     constructor() {
         super("platformerScene");
         this.my = {sprite: {}};
-
     }
 
     init() {
@@ -38,6 +37,126 @@ class Platformer extends Phaser.Scene {
             collides: true
         });
 
+        this.coins = this.map.createFromObjects("Coin", {
+            name: "Coin",
+            key: "tilemap_sheet",
+            frame: 151
+        });
+        this.physics.world.enable(this.coins, Phaser.Physics.Arcade.STATIC_BODY);
+        this.coinGroup = this.add.group(this.coins);
+
+        this.keys = this.map.createFromObjects("Key", {
+            name: "Key", 
+            key: "tilemap_sheet", 
+            frame: 27
+        });
+        this.physics.world.enable(this.keys, Phaser.Physics.Arcade.STATIC_BODY);
+        this.keyGroup = this.add.group(this.keys);
+
+        this.locks = this.map.createFromObjects("Lock", {
+            name: "Lock", 
+            key: "tilemap_sheet", 
+            frame: 28
+        });
+        this.physics.world.enable(this.locks, Phaser.Physics.Arcade.STATIC_BODY);
+        this.lockGroup = this.add.group(this.locks);
+
+        this.ropes1 = this.map.createFromObjects("Ropes 1", {
+            name: "Ropes", 
+            key: "tilemap_sheet", 
+            frame: 89
+        });
+        this.physics.world.enable(this.ropes1, Phaser.Physics.Arcade.STATIC_BODY);
+        this.ropes1Group = this.add.group(this.ropes1);
+
+        this.ropes2 = this.map.createFromObjects("Ropes 2", {
+            name: "Ropes", 
+            key: "tilemap_sheet", 
+            frame: 89
+        });
+        this.physics.world.enable(this.ropes2, Phaser.Physics.Arcade.STATIC_BODY);
+        this.ropes2Group = this.add.group(this.ropes2);
+
+        this.ropes3 = this.map.createFromObjects("Ropes 3", {
+            name: "Ropes", 
+            key: "tilemap_sheet", 
+            frame: 89
+        });
+        this.physics.world.enable(this.ropes3, Phaser.Physics.Arcade.STATIC_BODY);
+        this.ropes3Group = this.add.group(this.ropes3);
+
+        this.ropes4 = this.map.createFromObjects("Ropes 4", {
+            name: "Ropes", 
+            key: "tilemap_sheet", 
+            frame: 89
+        });
+        this.physics.world.enable(this.ropes4, Phaser.Physics.Arcade.STATIC_BODY);
+        this.ropes4Group = this.add.group(this.ropes4);
+
+        this.ladders = this.map.createFromObjects("Ladders", {
+            name: "Ladders", 
+            key: "tilemap_sheet", 
+            frame: 71
+        });
+        this.physics.world.enable(this.ladders, Phaser.Physics.Arcade.STATIC_BODY);
+        this.laddersGroup = this.add.group(this.ladders);
+
+        this.antiFall = this.map.createFromObjects("Anti Fall", {
+            name: "Anti Fall",
+            key: "tilemap_sheet2",
+            frame: 87
+        });
+        this.physics.world.enable(this.antiFall, Phaser.Physics.Arcade.STATIC_BODY);
+        this.antiFallGroup = this.add.group(this.antiFall);
+
+        this.jumpBoost = this.map.createFromObjects("Jump Boost", {
+            name: "Jump Boost",
+            key: "tilemap_sheet2",
+            frame: 89
+        });
+        this.physics.world.enable(this.jumpBoost, Phaser.Physics.Arcade.STATIC_BODY);
+        this.jumpBoostGroup = this.add.group(this.jumpBoost);
+
+        this.pipe1 = this.map.createFromObjects("Pipes 1", {
+            name: "Pipes",
+            key: "tilemap_sheet",
+            frame: 134
+        });
+        this.physics.world.enable(this.pipe1, Phaser.Physics.Arcade.STATIC_BODY);
+        this.pipe1Group = this.add.group(this.pipe1);
+
+        this.pipe2 = this.map.createFromObjects("Pipes 2", {
+            name: "Pipes",
+            key: "tilemap_sheet",
+            frame: 95
+        });
+        this.physics.world.enable(this.pipe2, Phaser.Physics.Arcade.STATIC_BODY);
+        this.pipe2Group = this.add.group(this.pipe2);
+
+        this.pipe3 = this.map.createFromObjects("Pipes 3", {
+            name: "Pipes",
+            key: "tilemap_sheet",
+            frame: 95
+        });
+        this.physics.world.enable(this.pipe3, Phaser.Physics.Arcade.STATIC_BODY);
+        this.pipe3Group = this.add.group(this.pipe3);
+
+        this.pipe4 = this.map.createFromObjects("Pipes 4", {
+            name: "Pipes",
+            key: "tilemap_sheet",
+            frame: 95
+        });
+        this.physics.world.enable(this.pipe4, Phaser.Physics.Arcade.STATIC_BODY);
+        this.pipe4Group = this.add.group(this.pipe4);
+
+        this.win = this.map.createFromObjects("Win", {
+            name: "Win",
+            key: "tilemap_sheet",
+            frame: 112
+        });
+        this.physics.world.enable(this.win, Phaser.Physics.Arcade.STATIC_BODY);
+        this.winGroup = this.add.group(this.win);
+
         let tileSize = 18;  // Size of each tile
         let scaleFactor = this.SCALE;  // Scaling factor
         let tilesUp = 3;  // Number of tiles up from the bottom
@@ -62,7 +181,7 @@ class Platformer extends Phaser.Scene {
             this.physics.world.debugGraphic.clear()
         }, this);
 
-        // TODO: add camera code here
+        // add camera code here
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
         this.cameras.main.startFollow(my.sprite.player, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
         this.cameras.main.setDeadzone(50, 50);
@@ -74,19 +193,19 @@ class Platformer extends Phaser.Scene {
 
     update() {
         if(cursors.left.isDown) {
-            // TODO: have the player accelerate to the left
+            // have the player accelerate to the left
             my.sprite.player.body.setAccelerationX(-this.ACCELERATION);
             my.sprite.player.resetFlip();
             my.sprite.player.anims.play('walk', true);
 
         } else if(cursors.right.isDown) {
-            // TODO: have the player accelerate to the right
+            // have the player accelerate to the right
             my.sprite.player.body.setAccelerationX(this.ACCELERATION);
             my.sprite.player.setFlip(true, false);
             my.sprite.player.anims.play('walk', true);
 
         } else {
-            // TODO: set acceleration to 0 and have DRAG take over
+            // set acceleration to 0 and have DRAG take over
             my.sprite.player.body.setAccelerationX(0);
             my.sprite.player.body.setDragX(this.DRAG);
             my.sprite.player.anims.play('idle');
@@ -98,7 +217,7 @@ class Platformer extends Phaser.Scene {
             my.sprite.player.anims.play('jump');
         }
         if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
-            // TODO: set a Y velocity to have the player "jump" upwards (negative Y direction)
+            // set a Y velocity to have the player "jump" upwards (negative Y direction)
             my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
         }
 
