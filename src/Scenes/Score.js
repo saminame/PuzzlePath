4c56fc7e0e21fff895cd3970913c20cbe41ff62f
saminame <151252937+saminame@users.noreply.github.com>
2024-06-12 07:44:08 -0700

    added a Diamond tracker

--- src/Scenes/Score.js
+++ src/Scenes/Score.js
@@ -1,15 +1,17 @@
 class Score extends Phaser.Scene{
     constructor ()
     {
-        super({ key: 'scoreSceme', active: true });
+        super({ key: 'scoreScene', active: true });
 
     }
     init(){
         this.SCORE = 0;
+        this.DIAMONDS = 0;
     }
 
     create(){
         const scoreText = this.add.text(250, 10, '', { font: '48px Arial', fill: '#ffffff' });
+        const diamondText = this.add.text(250, 50, '', { font: '48px Arial', fill: '#ffffff' });
         const keyText = this.add.text(500, 10, '', { font: '48px Arial', fill: '#ffffff' });
         const ourGame = this.scene.get('platformerScene');
 
@@ -17,6 +19,12 @@ class Score extends Phaser.Scene{
         {
             this.SCORE += 1;
             scoreText.setText(`Score: ${this.SCORE}`);
+
+        }, this);
+        ourGame.events.on('DiamondTracker', function ()
+        {
+            this.DIAMONDS += 1;
+            diamondText.setText(`Diamonds: ${this.DIAMONDS}`);
 
         }, this);
         ourGame.events.on('KeyTracker', function ()
@@ -27,6 +35,7 @@ class Score extends Phaser.Scene{
         ourGame.events.on('restartScore', function ()
         {
             scoreText.setText("");
+            diamondText.setText("");
             keyText.setText("");
 
 
@@ -35,6 +44,7 @@ class Score extends Phaser.Scene{
         {
             this.SCORE = 0;
             scoreText.setText("Score: 0");
+            diamondText.setText("Diamonds: 0");
             keyText.setText("");
 
         }, this);
