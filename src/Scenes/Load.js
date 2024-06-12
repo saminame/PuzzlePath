c485292dc34f521c47479c4e18282664ee9aa921
saminame <151252937+saminame@users.noreply.github.com>
2024-06-12 07:45:00 -0700

    fixed Load
    animations were giving an error on console

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -25,46 +25,49 @@ class Load extends Phaser.Scene {
         });
 
         // Load sounds
-    this.load.audio('doorOpen', 'doorOpen_1.ogg');
-    this.load.audio('coinPick', 'jingles_NES09.ogg');
-    this.load.audio('restartSound', 'jingles_NES15.ogg');
-    this.load.audio('winSound', 'jingles_NES15.ogg');
-    this.load.audio('antiFall', 'jingles_NES06.ogg');
-    this.load.audio('jumpBoost', 'jingles_NES08.ogg');
-
-
+        this.load.audio('doorOpen', 'doorOpen_1.ogg');
+        this.load.audio('coinPick', 'jingles_NES09.ogg');
+        this.load.audio('restartSound', 'jingles_NES15.ogg');
+        this.load.audio('winSound', 'jingles_NES15.ogg');
+        this.load.audio('antiFall', 'jingles_NES06.ogg');
+        this.load.audio('jumpBoost', 'jingles_NES08.ogg');
     }
 
     create() {
-        this.anims.create({
-            key: 'walk',
-            frames: this.anims.generateFrameNames('platformer_characters', {
-                prefix: "tile_",
-                start: 0,
-                end: 1,
-                suffix: ".png",
-                zeroPad: 4
-            }),
-            frameRate: 15,
-            repeat: -1
-        });
+        if (!this.anims.get('walk')) {
+            this.anims.create({
+                key: 'walk',
+                frames: this.anims.generateFrameNames('platformer_characters', {
+                    prefix: "tile_",
+                    start: 0,
+                    end: 1,
+                    suffix: ".png",
+                    zeroPad: 4
+                }),
+                frameRate: 15,
+                repeat: -1
+            });
+        }
+        if (!this.anims.get('idle')) {
+            this.anims.create({
+                key: 'idle',
+                defaultTextureKey: "platformer_characters",
+                frames: [
+                    { frame: "tile_0000.png" }
+                ],
+                repeat: -1
+            });
+        }
 
-        this.anims.create({
-            key: 'idle',
-            defaultTextureKey: "platformer_characters",
-            frames: [
-                { frame: "tile_0000.png" }
-            ],
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'jump',
-            defaultTextureKey: "platformer_characters",
-            frames: [
-                { frame: "tile_0001.png" }
-            ],
-        });
+        if (!this.anims.get('jump')) {
+            this.anims.create({
+                key: 'jump',
+                defaultTextureKey: "platformer_characters",
+                frames: [
+                    { frame: "tile_0001.png" }
+                ],
+            });
+        }
 
          // ...and pass to the next Scene
          this.scene.start("platformerScene");
